3a77aadbcb3d69f9870a5ce07b109a6b71bbea21
Sharif Haason <ssh128@scarletmail.rutgers.edu>
2021-12-05 10:33:07 -0500

    day 5

---
+++ src/day5.rs
@@ -0,0 +1,100 @@
+use crate::common;
+use color_eyre::Report;
+use itertools::Itertools;
+use std::cmp;
+
+pub fn p1(path: &str) -> Result<i32, Report> {
+    let data = common::read_str_file(path)?;
+    let mut diagram = vec!([0; 1000]; 1000);
+    for d in data {
+        //info!("{}", d);
+        let v: Vec<&str> = d.split(" -> ").collect();
+        let (x1, y1) = v[0].split(',').collect_tuple().unwrap();
+        let (x2, y2) = v[1].split(',').collect_tuple().unwrap();
+        let (x1, y1, x2, y2) = (
+            x1.parse::<usize>()?,
+            y1.parse::<usize>()?,
+            x2.parse::<usize>()?,
+            y2.parse::<usize>()?,
+        );
+        let (dx, dy) = (usize::abs_diff(x1, x2), usize::abs_diff(y1, y2));
+        if dx == 0 {
+            for i in cmp::min(y1, y2)..=cmp::max(y1, y2) {
+                diagram[x1][i] += 1;
+            }
+        } else if dy == 0 {
+            for i in cmp::min(x1, x2)..=cmp::max(x1, x2) {
+                diagram[i][y1] += 1;
+            }
+        }
+        //info!("{}", dx);
+        //println!("{:?}", v);
+    }
+    let mut n = 0;
+    for r in diagram {
+        for c in r {
+            if c >= 2 {
+                n += 1;
+            }
+        }
+    }
+    //println!("{:?}", diagram);
+    Ok(n)
+}
+pub fn p2(path: &str) -> Result<i32, Report> {
+    let data = common::read_str_file(path)?;
+    let mut diagram = vec!([0; 1000]; 1000);
+    for d in data {
+        //info!("{}", d);
+        let v: Vec<&str> = d.split(" -> ").collect();
+        //info!("{}", v[0]);
+        let (x1, y1) = v[0].split(',').collect_tuple().unwrap();
+        let (x2, y2) = v[1].split(',').collect_tuple().unwrap();
+        let (x1, y1, x2, y2) = (
+            x1.parse::<usize>()?,
+            y1.parse::<usize>()?,
+            x2.parse::<usize>()?,
+            y2.parse::<usize>()?,
+        );
+        let (dx, dy) = (usize::abs_diff(x1, x2), usize::abs_diff(y1, y2));
+        if dx == 0 {
+            for i in cmp::min(y1, y2)..=cmp::max(y1, y2) {
+                diagram[x1][i] += 1;
+            }
+        } else if dy == 0 {
+            for i in cmp::min(x1, x2)..=cmp::max(x1, x2) {
+                diagram[i][y1] += 1;
+            }
+        } else if dx == dy {
+            if x1 < x2 && y1 < y2 {
+                for i in 0..=dx {
+                    diagram[x1 + i][y1 + i] += 1;
+                }
+            } else if x1 < x2 && y1 > y2 {
+                for i in 0..=dx {
+                    diagram[x1 + i][y1 - i] += 1;
+                }
+            } else if x1 > x2 && y1 < y2 {
+                for i in 0..=dx {
+                    diagram[x1 - i][y1 + i] += 1;
+                }
+            } else if x1 > x2 && y1 > y2 {
+                for i in 0..=dx {
+                    diagram[x1 - i][y1 - i] += 1;
+                }
+            }
+        }
+        //info!("{}", dx);
+        //println!("{:?}", v);
+    }
+    let mut n = 0;
+    for r in diagram {
+        for c in r {
+            if c >= 2 {
+                n += 1;
+            }
+        }
+    }
+    //println!("{:?}", diagram);
+    Ok(n)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(array_windows)]
 #![feature(array_zip)]
+#![feature(int_abs_diff)]
 
 use color_eyre::Report;
 use tracing::info;
@@ -9,6 +10,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() -> Result<(), Report> {
     common::log_setup()?;
@@ -20,5 +22,7 @@ fn main() -> Result<(), Report> {
     info!("Day 3, Part 2: {}", day3::p2("day3.txt")?);
     info!("Day 4, Part 1: {}", day4::p1("day4.txt")?);
     info!("Day 4, Part 2: {}", day4::p2("day4.txt")?);
+    info!("Day 5, Part 1: {}", day5::p1("day5.txt")?);
+    info!("Day 5, Part 2: {}", day5::p2("day5.txt")?);
     Ok(())
 }
